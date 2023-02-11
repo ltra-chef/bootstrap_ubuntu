13a32d4a848de9873991e478d6fa009a9f77439a
ltra-chef <98295883+ltra-chef@users.noreply.github.com>
2023-02-11 11:31:21 +1030

    initial

---
+++ bootstrap_ubuntu.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+
+#
+# Installs oh my zsh
+#
+install_oh_my_zsh() {
+    if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+
+    if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    fi
+
+    if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    fi
+
+    sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+}
+
+
+function install_apt_packages() {
+    PACKAGES=(
+        curl
+        git
+        terminator
+        openvpn
+        vlc
+        veracrypt
+        zsh
+        synaptic
+        papirus-icon-theme
+        ca-certificates
+    )
+
+    sudo apt install -y ${PACKAGES[@]}
+    sudo apt install -y ubuntu-restricted-extras ubuntu-restricted-addons
+}
+
+function install_snap_packages() {
+    PACKAGES=(
+        brave
+        chromium
+        mailspring
+        bitwarden
+        postman
+        spotify
+        simplenote
+    )
+
+    for package in "${PACKAGES[@]}"
+    do
+        sudo snap install -y $package
+    done
+
+    PACKAGES=(
+        code
+    )
+
+    for package in "${PACKAGES[@]}"
+    do
+        sudo snap install -y $package --classic
+    done
+}
+
+
+
+
+function install_packages()
+{
+  echo "Installing packages"
+  
+  apt install -y python3 python3-pip
+  apt install -y build-essential libssl-dev libffi-dev python3-dev
+  apt install -y  python3-dev
+  
+  install_apt_packages
+  install_snap_packages
+
+  install_oh_my_zsh
+  
+}
+
+function update_and_upgrade()
+{
+  echo "Update and Upgrade"
+
+  apt-get update -y
+  apt-get upgrade -y
+}
+
+function  root_or_die()
+{
+  if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+  fi
+}
+
+root_or_die
+update_and_upgrade
+install_packages
